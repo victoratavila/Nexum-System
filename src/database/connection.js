1c1b425d31544a213926f55b2c018c7030ae4211
Victor Atavila <victoratavila@hotmail.com>
2020-07-31 19:06:08 -0300

    Updated timezone

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,6 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('nexumsystem','victoratavila','Vik5e82w43', {
 host: 'mysql669.umbler.com',
 dialect: 'mysql',
-timezone: '-03:00',
 port: 41890
 });
 
