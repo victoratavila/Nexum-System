5e731fafdbb1f32c9ad770ccc46f5fc2e50317a5
Victor Atavila <victoratavila@hotmail.com>
2020-10-29 09:26:57 -0300

    .env orientantion

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('api','root','', {
-host: 'localhost',
+const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+host: DB_HOST,
 dialect: 'mysql',
 });
 
